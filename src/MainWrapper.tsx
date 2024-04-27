d3aa8afbd957bfcca262fedebfbe802c45e12705
Vitaliti Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-04-27 13:12:18 +0300

    feat: change app title name
    refactor: create mainWrapper components for all app wrappers
    style: add app favicon

---
+++ src/MainWrapper.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'src/redux/store.ts';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+type MainWrapperProps = {
+	children: ReactNode;
+};
+
+export const MainWrapper = ({ children }: MainWrapperProps) => {
+	return (
+		<>
+			<React.StrictMode>
+				<Provider store={store}>
+					<PersistGate loading={null} persistor={persistor}>
+						<BrowserRouter>{children}</BrowserRouter>
+					</PersistGate>
+				</Provider>
+			</React.StrictMode>
+			<ToastContainer />
+		</>
+	);
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'src/redux/store.ts';
 import './scss/App.scss';
+import { MainWrapper } from './MainWrapper.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</PersistGate>
-		</Provider>
-	</React.StrictMode>
+	<MainWrapper>
+		<App />
+	</MainWrapper>
 );
